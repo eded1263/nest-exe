0223d171dd6684626439243656f580ac061c0b84
Edilson <edilson@quicktech.no>
2023-09-19 14:28:31 -0300

    feat: Modificado o projeto para utilizar o nestjs!

--- src/app.ts
+++
@@ -1,1 +0,0 @@
-export default { logString: 'Hello, World!' }
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,45 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
+const ignorePlugin = new webpack.IgnorePlugin({
+  checkResource(resource) {
+    const lazyImports = [
+      '@nestjs/microservices',
+      // ADD THIS
+      '@nestjs/microservices/microservices-module',
+      '@nestjs/websockets',
+      // AND THIS
+      '@nestjs/websockets/socket-module',
+      '@nestjs/platform-express',
+      'cache-manager',
+      'class-validator',
+      'class-transformer'
+    ];
 
+    if (!lazyImports.includes(resource)) {
+      return false;
+    }
+    try {
+      require.resolve(resource);
+    } catch (err) {
+      return true;
+    }
+    return false;
+  }
+});
 module.exports = {
   mode: 'production',
-  entry: {
-    server: './build/src/index.js',
-  },
+  entry: ['regenerator-runtime/runtime.js', './build/main.js'],
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   target: 'node',
   node: {
     __dirname: false,
-    __filename: false,
+    __filename: false
   },
-}
+  stats: {
+    errorDetails: true
+  },
+  plugins: [ignorePlugin]
+};
